8806c0acc46de44d9a747253955230fb467cab20
Ysabel <ysabel.garcia@live.com>
2021-09-30 23:52:49 -0600

    showed appointment data on webpack-dev-server environment

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import "components/Application.scss";
 
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 function Application(props) {
   const [day, setDay] = useState("Monday");
@@ -25,6 +26,61 @@ function Application(props) {
     },
   ];
 
+  const appointments = [
+    {
+      id: 1,
+      time: "12pm",
+    },
+    {
+      id: 2,
+      time: "1pm",
+      interview: {
+        student: "Lydia Miller-Jones",
+        interviewer: {
+          id: 1,
+          name: "Sylvia Palmer",
+          avatar: "https://i.imgur.com/LpaY82x.png",
+        }
+      }
+    },
+    {
+      id: 3,
+      time: "2pm",
+    },
+    {
+      id: 4,
+      time: "3pm",
+      interview: {
+        student: "Archie Cohen",
+        interviewer: {
+          id: 2,
+          name: "Tori Malcolm",
+          avatar: "https://i.imgur.com/Nmx0Qxo.png",
+        }
+      }
+    },
+    {
+      id: 5,
+      time: "4pm",
+      interview: {
+        student: "Maria Boucher",
+        interviewer: {
+          id: 3,
+          name: "Mildred Nazir",
+          avatar: "https://i.imgur.com/T2WwVfS.png",
+        }
+      }
+    },
+  ];
+
+  const appointmentsList = appointments.map(appointment => {
+    return (
+      <Appointment 
+        key={appointment.id}
+        {...appointment}
+      />
+    )
+  })
   return (
     <main className="layout">
       <section className="sidebar">
@@ -48,7 +104,8 @@ function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentsList}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
