bc748b16459ff3045429e91f0f5ff040f3f3b687
Ysabel <ysabel.garcia@live.com>
2021-09-30 23:24:43 -0600

    added more components inside Appointment folder - incorporated appointment show/empty to index.js in Appt folder

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+
+function Form(props) {
+  const [ interviewer, setInterviewer ] = useState(props.interviewer || null);
+  const [ name, setName ] = useState(props.name || "");
+
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={name}
+            onChange={(event) => {setName(event.target.value)}}
+            /*
+              This must be a controlled component
+            */
+          />
+        </form>
+        <InterviewerList 
+          interviewers={props.interviewers} 
+          interviewer={interviewer} 
+          setInterviewer={setInterviewer} 
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
+        </section>
+      </section>
+    </main>
+  )
+};
+
+export default Form;
