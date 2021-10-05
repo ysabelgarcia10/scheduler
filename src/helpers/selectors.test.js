3d6011f2a44d15ce389f712c50f0411dbb88dddb
Ysabel <ysabel.garcia@live.com>
2021-10-04 19:59:01 -0600

    completed useVisualMode requirements - setMode not working fully, needs to be checked

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -91,27 +91,27 @@ test("getInterview returns null if no interview is booked", () => {
 
 //<------ getInterviewersforDay tests -------->
 test("getInterviewersForDay returns an array", () => {
-  const result = getAppointmentsForDay(state, "Monday");
+  const result = getInterviewersForDay(state, "Monday");
   expect(Array.isArray(result)).toBe(true);
 });
 
-test("getInterviewersForDay returns an array with a length matching the number of appointments for that day", () => {
-  const result = getAppointmentsForDay(state, "Monday");
+test("getInterviewersForDay returns an array with a length matching the number of interviewers for that day", () => {
+  const result = getInterviewersForDay(state, "Monday");
   expect(result.length).toEqual(3);
 });
 
-test("getInterviewersForDay returns an array containing the correct appointment objects", () => {
-  const [first, second] = getAppointmentsForDay(state, "Tuesday");
+test("getInterviewersForDay returns an array containing the correct interviewers objects", () => {
+  const [first, second] = getInterviewersForDay(state, "Tuesday");
   expect(first).toEqual(state.appointments["4"]);
   expect(second).toEqual(state.appointments["5"]);
 });
 
 test("getInterviewersForDay returns an empty array when the days data is empty", () => {
-  const result = getAppointmentsForDay({ days: [] }, "Monday");
+  const result = getInterviewersForDay({ days: [] }, "Monday");
   expect(result.length).toEqual(0);
 });
 
 test("getInterviewersForDay returns an empty array when the day is not found", () => {
-  const result = getAppointmentsForDay(state, "Wednesday");
+  const result = getInterviewersForDay(state, "Wednesday");
   expect(result.length).toEqual(0);
 });
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,33 +1,42 @@
 import { useState } from "react";
 
-function useVisualMode(initial) {
+export function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
   function transition(newMode, replace = false) {
-    setMode(newMode);
-
-    if (replace) {
-      const replaceHistory = [...history];
-      replaceHistory[replaceHistory.length - 1] = mode;
-      setHistory(replaceHistory);
-    } else {
-      const newHistory = [...history];
-      newHistory.push(newMode);
-      setHistory(newHistory);
-    }
+    
+    setHistory((prev) => {
+      let newHistory =[];
+      // WHY IS SETMODE NOT WORKING? ... :'(
+      setMode(newMode); //WHY CANT IT RUN PROPERLY WITH OR WITHOUT THIS?
+      
+      if (replace) {
+        newHistory = [...prev]
+        newHistory[newHistory.length - 1] = newMode;
+        return setHistory(newHistory);
+      } else {
+        newHistory = [...prev, newMode];
+        return setHistory(newHistory);
+      };
+    })
   }
 
   function back() { 
-    const newHistory = [...history];
-    newHistory.pop(mode);
 
-    if (history.length > 1)
-    setMode(newHistory[newHistory.length - 1])
-   }
+    setHistory((prev) => {
+      let newHistory = [...prev];
+      
+      if (history.length > 1) {
+        newHistory = [...prev].slice(0, history.length - 1);
+        setMode(newHistory[newHistory.length - 1]);
+      };
+      
+      setHistory(newHistory);
+    })
+  }
 
-  return {mode, transition, back}
 
+  // const current = history[history.length - 1];
+  return {mode, transition, back};
 };
-
-export default useVisualMode;
